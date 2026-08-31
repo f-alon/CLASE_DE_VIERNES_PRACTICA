c0e04544016a7f6f33db6c2c9ff634d321affd7a
f-alon <f.desananiceto@protonmail.com>
2026-08-31 18:12:19 -0300

    funciones_cine

---
+++ funciones_cine.py
@@ -0,0 +1,24 @@
+"""
+Un cine necesita un programa para decidir si una persona puede entrar y cuánto pagará la entrada.
+
+Reglas generales:
+
+    Edad mínima del cine:
+        Menores de 5 años no pueden entrar bajo ninguna circunstancia.
+
+    Clasificación de la película:
+        Cada película tiene una edad mínima (por ejemplo, 13).
+        Si la persona es menor a esa edad, no puede entrar.
+
+    Tipos de entrada según edad:
+        De 5 a 12 años → Infantil → $1000
+        De 13 a 17 años → Adolescente → $1500
+        Desde los 18 años → General → $2000
+
+    Descuento especial:
+        Los estudiantes tienen un 20% de descuento.
+
+ACLARACIÓN: se debe utilizar un archivo para definir las funciones y otro para el programa principal
+
+
+"""
